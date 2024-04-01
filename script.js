b20a114ebe7c002509f5dec94c475c693cf62518
amanpunia <71539683+aman99566@users.noreply.github.com>
2024-04-01 10:26:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["","Student","Aspiring Software Engineer", "Software tester", "Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
